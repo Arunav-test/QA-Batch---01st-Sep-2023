c06039775fd8cf19ea4bc189228dc8ef20869c79
Som <somaiah1988@gmail.com>
2023-10-17 22:08:18 -0400

    class programs

--- QA Class 29_Writing Selenium Test Cases -1/src/WebTable1.java
+++ QA Class 29_Writing Selenium Test Cases -1/src/WebTable1.java
@@ -13,19 +13,40 @@ WebDriver driver = new ChromeDriver();
 driver.manage().window().maximize();
 driver.get("https://omayo.blogspot.com/");
 
-List<WebElement> totalRows=driver.findElements(By.xpath("//table[@id='table1']/tbody/tr"));
-System.out.println(totalRows.size());
+WebElement table=driver.findElement(By.xpath("//table[@id='table1']"));
+List<WebElement> Rows =table.findElements(By.tagName("tr"));// contains row information
+System.out.println("total number of rows "+Rows.size());
 
-  String first="//table[@id='table1']/tbody/tr[";
-  String second="]";
-    
-    
-    for(int i=1;i<=totalRows.size();i++) {
-    WebElement rows=driver.findElement(By.xpath(first+i+second));
-    	String row=rows.getText();
-    	System.out.println(row);
+//finds number of columns
+
+     List<WebElement> row0= Rows.get(0).findElements(By.tagName("th"));
+	List<WebElement> row1= Rows.get(1).findElements(By.tagName("td"));
+     List<WebElement> row2= Rows.get(2).findElements(By.tagName("td"));
+	  List<WebElement> row3= Rows.get(3).findElements(By.tagName("td"));
+	  List<WebElement> row4= Rows.get(4).findElements(By.tagName("td"));
+	 
+  System.out.println(row0.size()); 
+	
+	  System.out.println(row1.size()); System.out.println(row2.size());
+	  System.out.println(row3.size()); System.out.println(row4.size());
+	 
+ 
+
+for(int i=0;i<row1.size();i++) {
+	System.out.print(row0.get(i).getText()+" | ");
+	
+	  System.out.print(row1.get(i).getText()+" | ");
+	  System.out.print(row2.get(i).getText()+" | ");
+	  System.out.print(row3.get(i).getText()+" | ");
+	  System.out.print(row4.get(i).getText()+" | "); 
+	  System.out.println();
+	 
+}
+
+
+
     	
-    }
+    
     
     
 
---
+++ QA Class 29_Writing Selenium Test Cases -1/src/WebTable4.java
@@ -0,0 +1,27 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebTable4 {
+
+	public  static void  searchTable(String name) throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		Thread.sleep(2000);
+		driver.get("https://www.hyrtutorials.com/p/add-padding-to-containers.html");
+		String firstXpath="//td[contains(text(),'";
+		String secondXpath="')]/preceding-sibling::td/input[@type='checkbox']";
+	
+		System.out.println(firstXpath+name+secondXpath);
+		Thread.sleep(5000);
+		driver.findElement(By.xpath(firstXpath+name+secondXpath)).click();
+		
+		
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+		searchTable("Maria Anders");
+
+	}
+
+}
