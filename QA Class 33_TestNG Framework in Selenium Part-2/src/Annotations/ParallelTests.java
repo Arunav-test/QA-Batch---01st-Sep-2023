1377b528d3472a1d702a7a288f764ee5231f66b1
Som <somaiah1988@gmail.com>
2023-10-24 21:44:05 -0400

    class programs

---
+++ QA Class 33_TestNG Framework in Selenium Part-2/src/Annotations/ParallelTests.java
@@ -0,0 +1,55 @@
+package Annotations;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class ParallelTests {
+	
+	@Test
+	public void login() throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.saucedemo.com/");
+		driver.findElement(By.xpath("//input[@id='user-name']")).sendKeys("standard_user");
+		driver.findElement(By.xpath("//input[@id='password']")).sendKeys("secret_sauce");
+		driver.findElement(By.xpath("//input[@id='login-button']")).click();
+		Thread.sleep(2000);
+		driver.quit();
+	}
+	@Test
+	public void logout() throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3000));
+		driver.get("https://www.saucedemo.com/");
+		driver.findElement(By.xpath("//input[@id='user-name']")).sendKeys("standard_user");
+		driver.findElement(By.xpath("//input[@id='password']")).sendKeys("secret_sauce");
+		driver.findElement(By.xpath("//input[@id='login-button']")).click();
+		
+		
+		driver.findElement(By.xpath("//button[@id='react-burger-menu-btn']")).click();
+		driver.findElement(By.xpath("//a[@id='logout_sidebar_link']")).click();
+		Thread.sleep(2000);
+		driver.quit();
+	}
+	@Test
+	public void cart() throws InterruptedException {
+			WebDriver driver = new ChromeDriver();
+			driver.manage().window().maximize();
+			driver.get("https://www.saucedemo.com/");
+			driver.findElement(By.xpath("//input[@id='user-name']")).sendKeys("standard_user");
+			driver.findElement(By.xpath("//input[@id='password']")).sendKeys("secret_sauce");
+			driver.findElement(By.xpath("//input[@id='login-button']")).click();
+			
+			driver.findElement(By.xpath("//button[@id='add-to-cart-sauce-labs-backpack']")).click();
+			driver.findElement(By.xpath("//a[@class='shopping_cart_link']")).click();
+			Thread.sleep(2000);
+			driver.quit();
+		}
+
+
+}
---
+++ QA Class 33_TestNG Framework in Selenium Part-2/src/Annotations/RetryLogic.java
@@ -0,0 +1,21 @@
+package Annotations;
+
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+public class RetryLogic implements IRetryAnalyzer{
+	int count=0;
+	int limit=5;
+
+	@Override
+	public boolean retry(ITestResult result) {
+		if(count<limit)
+		{
+		count++;
+		return true;
+    	}
+		
+		return false;
+	}
+
+}
